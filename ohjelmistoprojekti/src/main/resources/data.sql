af6efdb8ecae33e1280dec4b9d7e27dff36e7cf0
Topi Kettunen <topi@topikettunen.com>
2018-04-12 13:00:42 +0300

    Small edits to SQL queries

--- ohjelmistoprojekti/src/main/resources/data.sql
+++ ohjelmistoprojekti/src/main/resources/data.sql
@@ -3,14 +3,27 @@ VALUES
 ('Les Pasila'),
 ('Kari Grandit');
 
-INSERT INTO question (question, team_id)
+INSERT INTO question (question, question_type, team_id)
 VALUES
-("Saatko mielestäsi nopeasti yhteyttä helgaan tai ainejärjestöihin mieltäsi askarruttavissa asioissa?", 1),
-("Onko yhteyden ottaminen vaikeaa/työlästä?", 1),
-("Mitä kautta yleensä otat yhteyttä Helgaan tai opiskelijajärjestöihin?", 1);
+("Sukupuoli?", "avoin", 1),
+("Ikä?", "monivalinta", 1),
+("Millä kampuksella opiskelet?", "monivalinta", 1),
+("Monennen vuoden opiskelija olet?", "monivalinta", 1),
+("Miten arvioisit opiskelijajärjestöjen ja opiskelijoiden välisen vuorovaikutuksen? (face-to-face, sposti, puhelinnumerot)", "avoin", 1),
+("Koetko oppilaiden ja ainejärjestöjen välisen kommunikoinnin olevan riittävää (tarpeelliseksi)?", "monivalinta", 1),
+("Mitä kautta yleensä otat yhteyttä Helgaan tai opiskelijajärjestöihin?", "avoin", 1),
+("Millä tavalla haluaisit ottaa ainejärjestöihin mieluiten yhteyttä?", "monivalinta", 1),
+("Käyttäisitkö mahdollista chatbox-sovellusta ainejärjestöiden tavoittamiseksi, jos sellainen olisi olemassa?", "monivalinta", 1),
+("Pitäisikö chatbox-sovelluksen käyttö olla mahdollista anonyymisti?", "avoin", 1),
+("Onko mielestäsi ainejärjestöihin hankalaa ottaa yhteyttä/mitä voisi parantaa?", "avoin", 1);
 
-INSERT INTO question (question, team_id)
+INSERT INTO question (question, question_type, team_id)
 VALUES
-("Monettako vuotta opiskelet haaga-heliassa?", 2),
-("Syötkö koulussa välipalaa?", 2),
-("Ostatko juotavaa koulussa? Jos 'Joo' Kahvio vai Automaatti?", 2);
+("Monettako vuotta opiskelet haaga-heliassa?", "avoin", 2),
+("Ostatko koulussa virvokkeita?", "monivalinta",2),
+("Miksi valitset limukoneet?", "monivalinta", 2),
+("Onko koneita riittävästi ja onko ne sijoitettu hyvin?", "monivalinta", 2),
+("Käyttäisitkö juoma-automaatteja jos niistä saisi muutakin kuin limsaa ja energiajuomia?", "monivalinta", 2),
+("Miksi käytät kahviota?", "monivalinta", 2),
+("Toivoisitko laajempaa valikoimaa kahvioon?", "monivalinta", 2),
+("Onko kahviossa asioiminen nopeaa?", "monivalinta", 2);
--- ohjelmistoprojekti/src/main/resources/schema.sql
+++ ohjelmistoprojekti/src/main/resources/schema.sql
@@ -1,22 +1,30 @@
+/*
+CREATE DATABASE ohjelmistoprojekti
+CHARACTER SET utf8;
+
+GRANT ALL ON ohjelmistoprojekti.* TO 'java'@'localhost' IDENTIFIED by 'password';
+*/
+
 CREATE TABLE team (
 team_id int NOT NULL AUTO_INCREMENT,
 team_name varchar(1000) NOT NULL,
 PRIMARY KEY (team_id)
 );
 
+CREATE TABLE question (
+question_id int NOT NULL AUTO_INCREMENT,
+question varchar(1000) NOT NULL,
+question_type varchar(10000) NOT NULL,
+team_id int,
+PRIMARY KEY (question_id),
+FOREIGN KEY (team_id) REFERENCES team(team_id)
+);
+
 CREATE TABLE answer (
 answer_id int NOT NULL AUTO_INCREMENT,
 answer varchar(10000),
-PRIMARY KEY (answer_id)
-);
-
-CREATE TABLE question (
-id int NOT NULL AUTO_INCREMENT,
-question varchar(1000) NOT NULL,
-team_id int,
-answer_id int,
-PRIMARY KEY (id),
-FOREIGN KEY (team_id) REFERENCES team(team_id),
-FOREIGN KEY (answer_id) REFERENCES answer(answer_id)
+question_id int,
+PRIMARY KEY (answer_id),
+FOREIGN KEY (question_id) REFERENCES question(question_id)
 );
 
