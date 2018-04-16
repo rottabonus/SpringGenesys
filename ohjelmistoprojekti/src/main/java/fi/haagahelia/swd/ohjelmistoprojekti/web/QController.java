048453e971e92c650b2f086061fb293a5321fd41
Katajamäki Ilkka <a1700312@myy.haaga-helia.fi>
2018-04-16 14:34:25 +0300

    thymelaf

--- ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/web/QController.java
+++ ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/web/QController.java
@@ -3,21 +3,26 @@ package fi.haagahelia.swd.ohjelmistoprojekti.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import fi.haagahelia.swd.ohjelmistoprojekti.domain.Question;
 import fi.haagahelia.swd.ohjelmistoprojekti.domain.QuestionRepository;
 import fi.haagahelia.swd.ohjelmistoprojekti.domain.Team;
+import fi.haagahelia.swd.ohjelmistoprojekti.domain.TeamRepository;
 
 
-@RestController
+@Controller
 public class QController {
 	@Autowired
 	private QuestionRepository qrepository;
+	
+	@Autowired
+	private TeamRepository trepository;
 	
 	
 	// REST get all questions
@@ -38,8 +43,30 @@ public class QController {
 		return (List<Question>) team.getQuestion_list();
 	}
 	
-	//@RequestMapping("/question")
-	//public Question question(@RequestParam(value="jotain") )
+	@RequestMapping(value="/teamlist", method=RequestMethod.GET)
+	public String teamList(Model model){
+		model.addAttribute("teams", trepository.findAll());
+		return "teamlist";
+	}
 	
+	@RequestMapping(value="/add")
+	public String addTeam(Model model){
+		model.addAttribute("team", new Team());
+		return "newteam";
+	}
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(Team team){
+		trepository.save(team);
+		return "redirect:/teamlist";
+	}
+	
+	@RequestMapping(value="/questionlist/team/{id}", method=RequestMethod.GET)
+	public String questionlist(@PathVariable("id") Long teamId, Model model){
+		model.addAttribute("teams", trepository.findOne(teamId));
+		model.addAttribute("questions", qrepository.findAll());
+		return "questionlist";
+	}
+
 	
 }
