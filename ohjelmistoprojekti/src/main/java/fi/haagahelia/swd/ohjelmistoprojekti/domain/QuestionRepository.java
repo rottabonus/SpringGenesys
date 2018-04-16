59c3a9b2f3eee913ff0784878625d78689cfeea9
Hallenberg Felix <a1700342@myy.haaga-helia.fi>
2018-04-16 13:16:31 +0300

    Added POST for Answer by questionId and GET all answers

--- ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/domain/QuestionRepository.java
+++ ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/domain/QuestionRepository.java
@@ -1,7 +1,5 @@
 package fi.haagahelia.swd.ohjelmistoprojekti.domain;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuestionRepository extends CrudRepository<Question, Long>{
--- ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/web/QController.java
+++ ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/web/QController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.haagahelia.swd.ohjelmistoprojekti.domain.Answer;
+import fi.haagahelia.swd.ohjelmistoprojekti.domain.AnswerRepository;
 import fi.haagahelia.swd.ohjelmistoprojekti.domain.Question;
 import fi.haagahelia.swd.ohjelmistoprojekti.domain.QuestionRepository;
 import fi.haagahelia.swd.ohjelmistoprojekti.domain.Team;
@@ -18,7 +21,8 @@ import fi.haagahelia.swd.ohjelmistoprojekti.domain.Team;
 public class QController {
 	@Autowired
 	private QuestionRepository qrepository;
-	
+	@Autowired
+	private AnswerRepository arepository;
 	
 	// REST get all questions
 	@RequestMapping(value="/questions", method=RequestMethod.GET)
@@ -38,8 +42,21 @@ public class QController {
 		return (List<Question>) team.getQuestion_list();
 	}
 	
-	//@RequestMapping("/question")
-	//public Question question(@RequestParam(value="jotain") )
 	
+	//REST POST answer
+	@RequestMapping(value="/answer/{id}", method = RequestMethod.POST)
+	public @ResponseBody Answer createAnswer(@RequestBody Answer answer, @PathVariable("id") Question questionId) {
+		
+		System.out.println(answer.getAnswer());
+		answer.setQuestion(questionId);
+		arepository.save(answer);
+		return answer;
+	}
+	
+	//REST get all answers
+	@RequestMapping(value="/answers", method=RequestMethod.GET)
+	public @ResponseBody List<Answer> answerListRest() {	
+        return (List<Answer>) arepository.findAll();
+	}
 	
 }
