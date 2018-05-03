7fee3f418f847ce5b6aad093ea556956fc94887c
Holma Janne <a1700388@myy.haaga-helia.fi>
2018-05-03 12:20:20 +0300

    getAnswersBySurvey controller ready

--- ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/domain/ChoiceAnswerRepository.java
+++ ohjelmistoprojekti/src/main/java/fi/haagahelia/swd/ohjelmistoprojekti/domain/ChoiceAnswerRepository.java
@@ -7,14 +7,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface ChoiceAnswerRepository extends CrudRepository<ChoiceAnswer, Long>{
-// tässä koodissa on vielä jotain virhettä
-//	uusittua koodia
-//	SELECT choice_answer_id FROM choice_answer AS ca JOIN answer_option AS op ON ca.choice_answer_id=op.choice_answer_id JOIN question AS qu ON op.question_id=qu.question_id JOIN survey AS su ON
-//			qu.survey_id=su.survey_id WHERE su.survey_id=1;
-	@Query("SELECT option_id "
-			+ "FROM choice_answer AS ca JOIN option AS op ON ca.choice_answer_id=op.choice_answer_id "
-			+ "JOIN question AS qu ON op.question_id=qu.question_id JOIN survey AS su "
-			+ "ON qu.survey_id=su.survey_id"
+
+//	SELECT ca.answer_option_id FROM choice_answer AS ca 
+//	JOIN answer_option AS ao ON ca.answer_option_id=ao.answer_option_id
+//			JOIN question AS qu ON ao.question_id=qu.question_id
+//			JOIN survey AS su ON qu.survey_id=su.survey_id 
+//			WHERE su.survey_id=1;
+	@Query("SELECT ca.answer_option_id FROM choice_answer AS ca"
+			+ "JOIN answer_option AS ao ON ca.answer_option_id=ao.answer_option_id"
+			+ "JOIN question AS qu ON ao.question_id=qu.question_id"
+			+ "JOIN survey AS su ON qu.survey_id=su.survey_id "
 			+ "WHERE su.survey_id= :id")
 	ArrayList getChoiceAnswerListBySurvey(@Param("id") Long id);
 }
