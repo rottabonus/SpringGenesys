9e1e8a475a3a7d4f3393e90ad51c2ba22d959e9a
Topi Kettunen <topi@topikettunen.com>
2018-05-07 14:35:58 +0300

    Update data.sql

--- ohjelmistoprojekti/src/main/resources/data.sql
+++ ohjelmistoprojekti/src/main/resources/data.sql
@@ -10,17 +10,18 @@ VALUES
 
 INSERT INTO question (question_type_id, question, survey_id)
 VALUES
-(2, 'Sukupuoli?', 1),
+(1, 'Sukupuoli?', 1),
 (1, 'Ikä?', 1),
 (1, 'Millä kampuksella opiskelet?', 1),
 (1, 'Monennen vuoden opiskelija olet?', 1),
 (2, 'Miten arvioisit opiskelijajärjestöjen ja opiskelijoiden välisen vuorovaikutuksen? (face-to-face, sposti, puhelinnumerot)', 1),
 (1, 'Koetko oppilaiden ja ainejärjestöjen välisen kommunikoinnin olevan riittävää (tarpeelliseksi)?', 1),
 (2, 'Mitä kautta yleensä otat yhteyttä Helgaan tai opiskelijajärjestöihin?', 1),
-(1,'Millä tavalla haluaisit ottaa ainejärjestöihin mieluiten yhteyttä?', 1),
+(1, 'Millä tavalla haluaisit ottaa ainejärjestöihin mieluiten yhteyttä?', 1),
 (1, 'Käyttäisitkö mahdollista chatbox-sovellusta ainejärjestöiden tavoittamiseksi, jos sellainen olisi olemassa?', 1),
-(2, 'Pitäisikö chatbox-sovelluksen käyttö olla mahdollista anonyymisti?', 1),
-(2, 'Onko mielestäsi ainejärjestöihin hankalaa ottaa yhteyttä/mitä voisi parantaa?', 1);
+(1, 'Pitäisikö chatbox-sovelluksen käyttö olla mahdollista anonyymisti?', 1),
+(1, 'Onko mielestäsi ainejärjestöihin hankalaa ottaa yhteyttä?', 1),
+(2, 'Miten yhteydenottamista voitaisiin helpottaa?', 1);
 
 INSERT INTO question (question_type_id, question, survey_id)
 VALUES
@@ -33,6 +34,38 @@ VALUES
 (1, 'Toivoisitko laajempaa valikoimaa kahvioon?', 2),
 (2, 'Mitä haluaisit lisätä kahvilan valikoimaan?', 2),
 (1, 'Onko kahviossa asioiminen nopeaa?', 2);
+
+-- Les Pasila
+
+INSERT INTO answer_option (question_id, answer_option)
+VALUES
+(1, 'Mies'),
+(1, 'Nainen'),
+(1, 'Muu'),
+(2, '17-20'),
+(2, '21-25'),
+(2, '26-30'),
+(2, '30+'),
+(3, 'Pasila'),
+(3, 'Porvoo'),
+(3, 'Malmi'),
+(3, 'Haaga'),
+(3, 'Vierumäki'),
+(4, 'Ensimmäisen'),
+(4, 'Toisen'),
+(4, 'Kolmannen'),
+(4, 'Neljännen'),
+(4, 'Muu'),
+(6, 'Kyllä'),
+(6, 'Ei'),
+(8, 'Kasvokkain'),
+(8, 'Puhelimitse (puhelut, viestit)'),
+(8, 'Sähköposti'),
+(8, 'Somessa'),
+(9, 'Kyllä'),
+(9, 'Ei'),
+(10, 'Kyllä'),
+(10, 'Ei');
 
 -- Kari Grandit
 
